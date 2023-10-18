848a683194ab7bf01cea2e600aba04e42a146258
Neil Van Alino <nvq.alino@gmail.com>
2023-10-18 15:40:53 +0800

    add realm provider

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import {
   useFonts,
@@ -6,15 +6,14 @@ import {
   Montserrat_400Regular,
 } from "@expo-google-fonts/montserrat";
 import EStyleSheet from "react-native-extended-stylesheet";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
   HomeStackScreen,
   ProjectsStackScreen,
 } from "./components/StackScreens";
 import globalStyles from "./components/GlobalStyles";
-import { createProjectsTable } from "./services/projects";
-import dbConn from "./services/getDbConn";
+import { RealmProvider } from "@realm/react";
+import { schemas } from "./services/models";
 
 // initialization
 EStyleSheet.build();
@@ -26,18 +25,18 @@ export default function App() {
     Montserrat_400Regular,
   });
 
-  useEffect(() => createProjectsTable(dbConn), []);
-
   return (
     <NavigationContainer>
-      <SafeAreaProvider>
-        <SafeAreaView style={globalStyles.container}>
-          <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Home" component={HomeStackScreen} />
-            <Tab.Screen name="Projects" component={ProjectsStackScreen} />
-          </Tab.Navigator>
-        </SafeAreaView>
-      </SafeAreaProvider>
+      <RealmProvider schemas={schemas}>
+        <SafeAreaProvider>
+          <SafeAreaView style={globalStyles.container}>
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+              <Tab.Screen name="Home" component={HomeStackScreen} />
+              <Tab.Screen name="Projects" component={ProjectsStackScreen} />
+            </Tab.Navigator>
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </RealmProvider>
     </NavigationContainer>
   );
 }
